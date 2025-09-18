4b4d6da731ef0cafed1ed44ea9ff2872d555aa41
Ignacio Nicolás Correa Rodríguez <incorrearodriguez@mail.austral.edu.ar>
2025-09-18 20:23:03 -0300

    Deleted unused files.

--- components/realtime-chat-interface.tsx
+++ components/realtime-chat-interface.tsx
@@ -1,5 +1,5 @@
 'use client';
-
+// TO BE DELETED
 import React from 'react';
 import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
